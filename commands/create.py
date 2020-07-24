97569a9c56b62b690e8f8ada60de738ceb62b6d8
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-07-24 16:37:04 +0200

    added more of create

--- commands/create.py
+++ commands/create.py
@@ -1,4 +1,27 @@
 from os import mkdir
+from json import dump
+from wget import download
+
+def mkfile(file_path):
+    with open(file_path, "w") as _:
+        pass
+
 
 def create(package_name):
     mkdir(package_name)
+    mkdir(package_name + "/Sources")
+    mkdir(package_name + "/Headers")
+    mkfile(package_name + "/Headers/__main__.jlh")
+    barebones_info = {
+        "Version": "1.0",
+        "Supported Version": "",
+        "Dependencies": []
+    }
+
+    download("https://raw.githubusercontent.com/Zorz42/jaclang/master/include/version.h", "newestjaclangversion.txt", bar=None)
+    with open("newestjaclangversion.txt") as newest_version:
+        newestjaclangversion = [line.split(" ")[2] for line in newest_version.read().split("\n")
+                                if len(line.split(" ")) == 3]
+        print(newestjaclangversion)
+    with open(package_name + "/Info.json", "w") as info_file:
+        dump(barebones_info, info_file)
